910ba3707c8c6fbccb330b1f7901ed80dbbb1e90
Roberto Mandekić <rmandekic21@foi.hr>
2023-04-25 18:04:49 +0200

    doređena forma za priojavu

--- EvaluationManagerG11/frmLogin.cs
+++ EvaluationManagerG11/frmLogin.cs
@@ -10,11 +10,30 @@ using System.Windows.Forms;
 
 namespace EvaluationManagerG11 {
     public partial class frmLogin : Form {
+        string username = "nastavnik";
+        string password = "test";
         public frmLogin() {
             InitializeComponent();
         }
 
         private void txtUsername_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e) {
+            if (txtUsername.Text == "") {
+                MessageBox.Show("Korisničko ime nije unešeno!", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else if (txtPassword.Text == "") {
+                MessageBox.Show("Lozinka nije unešena!", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else { 
+                if(txtUsername.Text == username && txtPassword.Text == password) {
+                    MessageBox.Show("Dobro došli!", "Prijavljeni ste!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    frmStudent frmStudents = new frmStudent();
+                    Hide(); 
+                    frmStudents.ShowDialog();
+                }
+
+            }
 
         }
     }
